871acb029e1b749a6bf5a7a3b9d72c96f6dca226
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-06 16:35:51 +0100

    feat: exercise 2 completed

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const namesFilter = specLet(names);
+console.log(namesFilter);
 
 // Invoca la funzione qui e stampa il risultato in console
 
+/**
+ * individuare elementi con la stessa lettera iniziale
+ * @param {string} arrayNames
+ * @returns {string}
+ */
+function specLet(arrayNames) {
+
+    const namesFilter = [];
+
+    for (let i = 0; i < arrayNames.length; i++) {
+        if (arrayNames[i][0] === "A") {     // controlla che A sia la prima lettera
+            namesFilter.push(arrayNames[i]);
+        }
+    }
+    return namesFilter
+}
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]

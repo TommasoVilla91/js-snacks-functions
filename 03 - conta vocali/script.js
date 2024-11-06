148b505d9961a1197170a4a8f859ca3012c13fb4
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-06 17:25:56 +0100

    feat: exercise 3 completed

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,27 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const letterFilter = vowels(word);
+console.log(letterFilter);
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+/**
+ * individuare vocali 
+ * @param {string} letter
+ * @returns {string}
+ */
+function vowels(letter) {
 
+    let letterFilter = "";
+
+    for (let i = 0; i < letter.length; i++) {
+        if (letter[i] === `a` || letter[i] === `e` || letter[i] === `i` || letter[i] === `o` || letter[i] === `u`) {
+            letterFilter += letter[i] + " ";
+        }
+    }
+    return letterFilter
+}
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
